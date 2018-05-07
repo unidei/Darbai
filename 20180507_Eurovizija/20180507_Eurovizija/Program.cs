1174f02dfb194eb9718b024e2b1417515a47abc3
unidei <38734493+unidei@users.noreply.github.com>
2018-05-07 21:32:45 +0300

    Add files via upload

---
+++ 20180507_Eurovizija/20180507_Eurovizija/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;//prisidedame sita//prisidedame prie references paspaudus desini "system windows forms"//pazymime
+using System.IO;//prisidedame sita
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20180507_Eurovizija
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string path = null;
+            var ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                path = ofd.FileName;//cia nurodome is kur imame duomenis somis4-iomis eilutemis
+                string temp = "Lithuania.txt";
+                char[] triminimas = temp.ToCharArray();//triminimas - yra partumpinimas
+                path = path.TrimEnd(triminimas);
+            }
+            List<List<int>> sarasai = new List<List<int>>();
+            foreach (var file in Directory.EnumerateFiles(path))
+            {
+                StreamReader reader = new StreamReader(file);//cia jau imame duomenis siomis eilutemis zemiau
+                List<int> duomenis = new List<int>();
+                string readAll = reader.ReadToEnd();
+                reader.Close();
+                string[] duomenys_atskirai = readAll.Split(' ');
+                foreach (var element in duomenys_atskirai)
+                {
+                    int t = Convert.ToInt32(element);
+                    duomenis.Add(t);
+                }
+                sarasai.Add(duomenis);
+                Console.WriteLine("Maziausiais balas yra: " + duomenis.Min());
+                Console.WriteLine("Didziausias balas yra: " + duomenis.Max());
+                Console.WriteLine("balų vidurkis yra: " + duomenis.Average());
+                Console.WriteLine("Balu suma yra: " +duomenis.Sum());
+
+            }
+            double maxAverage = double.MinValue;
+            foreach (var item in sarasai)
+            {
+                if (item.Average() > maxAverage)
+                    maxAverage = item.Average();
+            }
+            Console.WriteLine("Didziausias vidurkis yra: " + maxAverage);
+        }
+    }
+}
+            
+            
+            
+            
+            
+            
+            
+            //Console.WriteLine("Iveskite, kiek yra sarasu");
+            //int kiek = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 0; i < kiek; i++)
+            //{
+               //int n = Convert.ToInt32(Console.ReadLine());
+                //for (int i = 0; i < n; i++)
+                //{
+                    //Console.WriteLine("Iveskite balus");
+                    //foreach (int balas in balai)
+                    //{
+                       //Console.WriteLine(balas);
+
+                        //Console.WriteLine("Maziausiais balas yra: " + balai.Min());
+                        //Console.WriteLine("Didziausias balas yra: " + balai.Max());
+                        //Console.WriteLine("balų suma yra: " + balai.Average());
+                        //int balusuma[] = balai.Average());
+                    //}
+                //}
+            //Console.WriteLine("Maziausiais balas yra: " + balai.Min());
+            //}
+
+  
+        
+    
---
+++ 20180507_Klases/20180507_Klases/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20180507_Klases
+{
+    class Automobilis
+    {
+        public string Marke;
+        public string Modelis;
+        public int Gamybosmetai;
+        public int Rida;
+        public int Galingumas;
+        public string Spalva;
+
+        public double KiekPerMetusNuvaziuoja()
+        {
+            int praejoMetu = DateTime.Today.Year - Gamybosmetai;
+            return ((double)Rida /praejoMetu);
+        }
+
+    public Automobilis()//cia yra konstruktoriu, ir jo deka mes konstruojame objekta
+    {
+
+    }
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Automobilis auto = new Automobilis();
+        auto.Gamybosmetai = 1997;
+        auto.Marke = "VW";
+        auto.Modelis = "Passat";
+        auto.Rida = 287235;
+        Console.WriteLine(auto.KiekPerMetusNuvaziuoja());
+        }
+    }
+}
